d3af62ebf2286aa2da5a3a3a4a8be66b63c7f50e
mp3butcher <mp3butcher@gmail.com>
2015-11-02 18:34:23 +0100

    reboot

--- src/UIEditorModules/osg/osg_WinDebugNotifyHandlerQQQ.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef EDITOR_META_CLASS_osg_WinDebugNotifyHandlerQQQ_H
-#define EDITOR_META_CLASS_osg_WinDebugNotifyHandlerQQQ_H
-
-#include "Export.h"
-#include <MetaQQuickClass.h>
-#include <osg/Notify>
-
-#endif
-
